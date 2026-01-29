9fcfc667e867893b99e1637ad057a04dda6c715a
Murilo Pires <murilopiresoficial@gmail.com>
2026-01-29 18:32:33 -0300

    Importing and using CSS Modules files with component scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
   return (
     <>
-      <h1>Olá, Mundo!</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo est,
         laboriosam asperiores non dolorem natus quas praesentium iure suscipit
