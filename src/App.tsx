08f575b3e22f11789643d3e41083175d4077ecb9
Murilo Pires <murilopiresoficial@gmail.com>
2026-01-29 19:24:59 -0300

    Properties (props) and children of components in React

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='string'>Olá, Mundo1</Heading>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo est,
         laboriosam asperiores non dolorem natus quas praesentium iure suscipit
