6f71a5423ebd002823407524cba5c04b4b842813
Murilo Pires <murilopiresoficial@gmail.com>
2026-02-16 15:43:50 -0300

    Criando o componente de footer(rodapé)

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import type React from 'react';
+import styles from './styles.module.css';
+
+
+export function Footer() {
+  return <footer className={styles.footer}>
+    <a href="">Entenda como funciona a técnica Pomodoro</a>
+    <a href="">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito por @devmurilopires </a>
+  </footer>;
+}
