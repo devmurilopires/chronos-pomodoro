deff79912c4c1844ba8725b6dfddc098481c9073
Murilo Pires <murilopiresoficial@gmail.com>
2026-03-23 18:56:08 -0300

    API de contexto - useContext + useState (estado + contexto)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './styles/theme.css';
 import './styles/global.css';
+
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext';
+
 
 export function App() {
+
   return (
-    <Home />
+    <TaskContextProvider>
+      <Home />;
+    </TaskContextProvider>
   );
 }
